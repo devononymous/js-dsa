b4e11ef271a37a59d503606673aec8b4f738c8cb
devononymous <suloksushil@gmail.com>
2025-01-13 23:54:48 +0530

    shuffling added

---
+++ Array/shuffle.js
@@ -0,0 +1,26 @@
+// Write a javascript program to shuffle an array.
+
+ /*Psuedo code:
+
+fisher yeats shuffle algorithm
+1. start from the last element of the array
+2. pick a random element from the array
+3. swap the random element with the last element
+4. repeat the process for the remaining elements
+5. return the shuffled array
+
+
+*/
+
+let arr1 = [1, 52, 3, 44, 5, 86, 27, 8, 19, 110];
+
+function shuffleArray(arr) {
+        let shuffledArray = arr.slice();
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+                let j = Math.floor(Math.random() * (i + 1));
+                [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        }
+        return shuffledArray;
+}
+
+console.log(shuffleArray(arr1)); // [27, 3, 8, 110, 5, 86, 19, 4, 52, 1]
