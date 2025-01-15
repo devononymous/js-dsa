fc75e880b80694d7b3fb156fb86530fbf26eb6fe
devononymous <suloksushil@gmail.com>
2025-01-16 00:00:11 +0530

    added binary serach

---
+++ Array/binarySearch.js
@@ -0,0 +1,15 @@
+/* Write a  Jvascript program to perfrom a binary serach
+
+Note: A bnary serach or half-interval search algorithm finds the position of a specified input value within input value within an array
+ sorted by key value.
+
+
+ Sample array:
+
+  var items = [1.2.3.4.5.6.7,8.,9];
+  Expected Output:
+
+  console.log(binarySearch(items, 1)); //0
+  console.log(binarySearch(items, 5)); //4  */
+
+  
